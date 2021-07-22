7e2b0ac87b607bf55ac33db62b1306acdbd6c31e
Diane DELORD <82445395+DianeDelord@users.noreply.github.com>
2021-07-22 12:13:37 +0200

    3 premiers inputs - v2

---
+++ modal.js
@@ -0,0 +1,143 @@
+function editNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+    modalbg.style.display = "block";
+}
+
+// fermeture de la fenêtre formulaire
+const modalCloseBtn = document.getElementsByClassName("close");
+modalCloseBtn[0].addEventListener("click", closeModal);
+
+function closeModal() {
+    modalbg.style.display = "none";
+}
+
+// validation des champs de saisie de l'utilisateur 
+let formValidator = document.getElementById("reserve");
+
+// validation des champs prénom et nom de l'utilisateur 
+let verifyTextInput = /^[a-zA-Z-\s]{2,}$/;
+let firstInput = document.getElementById("first");
+let missingTextInputFirst = document.getElementById("validationFieldFirst");
+let secondInput = document.getElementById("last");
+let missingTextInputSecond = document.getElementById("validationFieldSecond");
+
+formValidator.addEventListener("submit", function(e) {
+    if (firstInput.value == "") {
+        missingTextInputFirst.innerHTML = "Vous devez saisir au moins deux caractères."
+        e.preventDefault();
+    } else if ((verifyTextInput.test(firstInput.value)) == false) {
+        missingTextInputFirst.innerHTML = "Vous devez saisir au moins deux caractères, les chiffres et caractères spéciaux ne sont pas acceptés.";
+        e.preventDefault();
+    }
+});
+formValidator.addEventListener("submit", function(e) {
+    if (secondInput.value == "") {
+        missingTextInputSecond.innerHTML = "Vous devez saisir au moins deux caractères."
+        e.preventDefault();
+    } else if ((verifyTextInput.test(secondInput.value)) == false) {
+        missingTextInputSecond.innerHTML = "Vous devez saisir au moins deux caractères, les chiffres et caractères spéciaux ne sont pas acceptés.";
+        e.preventDefault();
+    }
+});
+
+// validation de l'email : regex
+let verifyEmailInput = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let emailInput = document.getElementById("email");
+let missingEmailInput = document.getElementById("emailValidationField");
+
+formValidator.addEventListener("submit", function(e) {
+    if (emailInput.value == "") {
+        missingEmailInput.innerHTML = "Vous devez saisir une adresse mail."
+        e.preventDefault();
+    } else if ((verifyEmailInput.test(emailInput.value)) == false) {
+        missingEmailInput.innerHTML = "Vous devez saisir une adresse mail valide.";
+        e.preventDefault();
+    }
+});
+
+// validation du nombre de tournois
+// regex pas nécessaire car type="number" en html
+let quantityInput = document.getElementById("quantity");
+let missingQuantityInput = document.getElementById("quantityValidationField");
+
+formValidator.addEventListener("submit", function(e) {
+    if (quantityInput.value == "") {
+        missingQuantityInput.innerHTML = "Vous devez saisir un nombre entre 0 et 99."
+        e.preventDefault();
+    }
+});
+
+//validation villes - required sur le html fait tout péter :(
+
+//let inputRadioRequired = document.getElementById("location");
+//inputRadioRequired.setAttribute("required", "");
+//inputRadioRequired.required = true;
+
+let inputRadioRequired = document.getElementById("location");
+document.getElementById("location").required
+document.getElementById("location").getAttribute("required");
+
+
+let missingRadioButtonChecked = document.getElementById("radioButtonValidationField");
+formValidator.addEventListener("submit", function(e) {
+    if (inputRadioRequired.value == "") {
+        missingRadioButtonChecked.innerHTML = "Vous devez sélectionner au moins une ville."
+        e.preventDefault();
+    }
+});
+
+// quand l'utilisateur change de champ de saisie, js écoute l'autre champ texte
+
+firstInput.onchange = function(e) {
+    formValidator.addEventListener("submit", function(e) {
+        if (firstInput.value == "") {
+            missingTextInputFirst.innerHTML = "Vous devez saisir au moins deux caractères."
+            e.preventDefault();
+        } else if ((verifyTextInput.test(firstInput.value)) == false) {
+            missingTextInputFirst.innerHTML = "Vous devez saisir au moins deux caractères, les chiffres et caractères spéciaux ne sont pas acceptés.";
+            e.preventDefault();
+        }
+    });
+    missingTextInputFirst.innerHTML = "";
+};
+secondInput.onchange = function(e) {
+    formValidator.addEventListener("submit", function(e) {
+        if (secondInput.value == "") {
+            missingTextInputSecond.innerHTML = "Vous devez saisir au moins deux caractères."
+            e.preventDefault();
+        } else if ((verifyTextInput.test(secondInput.value)) == false) {
+            missingTextInputSecond.innerHTML = "Vous devez saisir au moins deux caractères, les chiffres et caractères spéciaux ne sont pas acceptés.";
+            e.preventDefault();
+        }
+    });
+    missingTextInputSecond.innerHTML = "";
+};
+emailInput.onchange = function(e) {
+    formValidator.addEventListener("submit", function(e) {
+        if (emailInput.value == "") {
+            missingEmailInput.innerHTML = "Vous devez saisir une adresse mail."
+            e.preventDefault();
+        } else if ((verifyEmailInput.test(emailInput.value)) == false) {
+            missingEmailInput.innerHTML = "Vous devez saisir une adresse mail valide.";
+            e.preventDefault();
+        }
+    });
+    missingEmailInput.innerHTML = "";
+};
